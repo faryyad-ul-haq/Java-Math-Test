e0619631c4e0a3aa9039011465437d47ff9540d5
Faryyad-ul-Haq <faryyad@DESKTOP-123456789>
2021-11-16 09:07:57 +0500

    created MyMathTest.java Junit file and added a math test testAdd()

---
+++ MathTest/src/test/java/MyMathTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+		MyMath mymath = new MyMath();
+		int addition = mymath.myAdd(5, 3);
+		assertEquals(8,addition);
+	}
+
+}
