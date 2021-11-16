efee4748b41cc7d4c25178eba20202357a466dfc
Faryyad-ul-Haq <faryyad@DESKTOP-123456789>
2021-11-16 08:48:42 +0500

    added functions mySubtract() myMultiply() myDivide() to MyMath.java

--- MathTest/src/test/java/MyMath.java
+++ MathTest/src/test/java/MyMath.java
@@ -4,5 +4,17 @@ public class MyMath {
 	public int myAdd(int a,int b) {
 		return a+b;
 	}
+	
+	public int mySubtract(int a,int b) {
+		return a-b;
+	}
+	
+	public int myMultiply(int a,int b) {
+		return a*b;
+	}
+	
+	public int myDivide(int a,int b) {
+		return a/b;
+	}
 
 }
