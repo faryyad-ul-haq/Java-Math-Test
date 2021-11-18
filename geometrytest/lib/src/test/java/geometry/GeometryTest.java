c2152e714e10faee36584a1e68e857adefcd100f
Faryyad-ul-Haq <faryyad@Desktop-123456789>
2021-11-18 22:23:21 +0500

    created geometrytest.java file and added required functions

--- geometrytest/lib/src/test/java/geometry/GeometryTest.java
+++ geometrytest/lib/src/test/java/geometry/GeometryTest.java
@@ -5,6 +5,35 @@ import org.testng.annotations.Test;
 
 public class GeometryTest {
   @Test
-  public void f() {
+  public void testareaRect() {
+	  
+	  geometry geo = new geometry();
+	  float area = geo.areaRect(5,4);
+	  Assert.assertEquals(20,area);
   }
+  
+  @Test
+  public void testPerimeterRect() {
+	  
+	  geometry geo = new geometry();
+	  float peri = geo.perimeterRect(5,4);
+	  Assert.assertEquals(20,peri);
+  }
+  
+  @Test
+  public void testVolumeSphere() {
+	  
+	  geometry geo = new geometry();
+	  float vol = geo.volumeSPhere(4);
+	  Assert.assertEquals(64,vol);
+  }
+  
+  @Test
+  public void testSUrfaceAreashpere() {
+	  
+	  geometry geo = new geometry();
+	  float sur = geo.surfaceAreaSphere(6);
+	  Assert.assertEquals(314.2857142857142857142857142857,sur);
+  }
+  
 }
